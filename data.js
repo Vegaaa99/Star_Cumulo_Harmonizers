94b769dd1dfa2cddf4f3800d191b145607712ab5
Valentina Vega <mavegac@unal.edu.co>
2024-10-05 17:55:43 -0500

    Adding archives questionary-Duolingo

---
+++ data.js
@@ -0,0 +1,74 @@
+const data = [
+  {
+    pic: 'hh',
+    Pregunta: 'What is the biggest and complex telescope that NASA has?',
+    engPhrase: 'James Webb Telescope',
+    phraseByWord: [
+      {
+        question: 'What is the biggest and complex telescope that NASA has?',
+        trans: ['James Webb Telescope'],
+        highlighted: true
+      },
+      {
+        question: 'The age of the light that the telescope can collect is',
+        trans: ['13.500 billion light years'],
+        highlighted: true
+      },
+      {
+        question: 'What instruments does the James Webb telescope use to obtain data?',
+        trans: ['Nircam, NIRSpec, MIRI, FGS/NIRISS'],
+        highlighted: true
+      },
+      {
+        question: 'What astronomical object does the image show?',
+        trans: ['A black hole, Two galaxies colliding, A Supernova, A new exoplanet'],
+        highlighted: true
+      },
+      {
+        question: 'What is this astronomical object?',
+        trans: ['A black hole, Two galaxies colliding, A Supernova, A new exoplanet'],
+        highlighted: true
+      }
+    ]
+  },
+  {
+    pic: './assets/olga-woman.svg',
+    frPhrase: `Il vient d'Australie`,
+    engPhrase: 'He comes from Australia',
+    phraseByWord: [
+      {
+        word: 'Il',
+        trans: ['he', 'him', 'it']
+      },
+      {
+        word: 'vient d',
+        trans: ['come from', 'is from', 'has just'],
+        highlighted: true
+      },
+      {
+        word: 'Australie',
+        trans: ['Australia']
+      }
+    ]
+  },
+  {
+    pic: './assets/victor-male.svg',
+    frPhrase: `Je suis canadien`,
+    engPhrase: 'I am Canadian',
+    phraseByWord: [
+      {
+        word: 'Je',
+        trans: ['I']
+      },
+      {
+        word: 'suis',
+        trans: ['am']
+      },
+      {
+        word: 'canadien',
+        trans: ['Canadian'],
+        highlighted: true
+      }
+    ]
+  }
+];
